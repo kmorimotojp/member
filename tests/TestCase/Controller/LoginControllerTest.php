6b69589a1ba0dc90962d50c25005528b18351f34
K.Morimoto <k.morimoto.real@gmail.com>
2018-04-25 18:13:20 +0900

    test

--- tests/TestCase/Controller/LoginControllerTest.php
+++ tests/TestCase/Controller/LoginControllerTest.php
@@ -11,15 +11,6 @@ class LoginControllerTest extends IntegrationTestCase
 {
 
     /**
-     * Fixtures
-     *
-     * @var array
-     */
-    public $fixtures = [
-        'app.login'
-    ];
-
-    /**
      * Test index method
      *
      * @return void
@@ -30,41 +21,11 @@ class LoginControllerTest extends IntegrationTestCase
     }
 
     /**
-     * Test view method
-     *
-     * @return void
-     */
-    public function testView()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
      * Test add method
      *
      * @return void
      */
     public function testAdd()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test edit method
-     *
-     * @return void
-     */
-    public function testEdit()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test delete method
-     *
-     * @return void
-     */
-    public function testDelete()
     {
         $this->markTestIncomplete('Not implemented yet.');
     }
